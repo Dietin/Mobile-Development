0c948f570bda15f062d79976624b8abc2b273d4a
Vincent <vincent01sj@gmail.com>
2023-06-09 16:53:37 +0700

    search before and on oke, tapi masih burik :v

--- app/src/main/java/academy/bangkit/capstone/dietin/ui/main_screen/search/SearchFragment.kt
+++ app/src/main/java/academy/bangkit/capstone/dietin/ui/main_screen/search/SearchFragment.kt
@@ -1,25 +1,22 @@
 package academy.bangkit.capstone.dietin.ui.main_screen.search
 
-import academy.bangkit.capstone.dietin.R
 import academy.bangkit.capstone.dietin.ui.main_screen.search.before.BeforeSearchFragment
 import academy.bangkit.capstone.dietin.databinding.FragmentSearchBinding
-import academy.bangkit.capstone.dietin.utils.Utils
-import academy.bangkit.capstone.dietin.utils.ViewModelFactory
+import academy.bangkit.capstone.dietin.ui.main_screen.search.on.OnSearchFragment
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 
 class SearchFragment : Fragment() {
 
     private var _binding: FragmentSearchBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var viewModel: SearchViewModel
-    private lateinit var loader: AlertDialog
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,18 +31,51 @@ class SearchFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-        requireActivity()
-            .supportFragmentManager
-            .beginTransaction()
-            .replace(binding.fragmentSearchHolder.id, BeforeSearchFragment())
-            .commit()
+        changeFragmentSearch(BeforeSearchFragment())
+        setupListener()
 
 
     }
+
+
+    private fun setupListener() {
+
+
+        binding.inputSearch.editText?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //do nothing
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                //do nothing
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (s?.length!! > 0) {
+                    changeFragmentSearch(OnSearchFragment())
+                }else{
+                    changeFragmentSearch(BeforeSearchFragment())
+                }
+            }
+        })
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+
+    private fun changeFragmentSearch(fragment : Fragment){
+        requireActivity()
+            .supportFragmentManager
+            .beginTransaction()
+            .replace(binding.fragmentSearchHolder.id, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+
+
+
 }
--- app/src/main/java/academy/bangkit/capstone/dietin/ui/main_screen/search/on/OnSearchFragment.kt
+++ app/src/main/java/academy/bangkit/capstone/dietin/ui/main_screen/search/on/OnSearchFragment.kt
@@ -1,4 +1,4 @@
-package academy.bangkit.capstone.dietin.ui.main_screen.search.before
+package academy.bangkit.capstone.dietin.ui.main_screen.search.on
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,8 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import academy.bangkit.capstone.dietin.R
 import academy.bangkit.capstone.dietin.data.remote.model.Recipe
-import academy.bangkit.capstone.dietin.databinding.FragmentBeforeSearchBinding
-import academy.bangkit.capstone.dietin.databinding.ItemFoodCard1Binding
+import academy.bangkit.capstone.dietin.databinding.FragmentOnSearchBinding
 import academy.bangkit.capstone.dietin.databinding.ItemFoodCard2Binding
 import academy.bangkit.capstone.dietin.ui.main_screen.home.HomeViewModel
 import academy.bangkit.capstone.dietin.utils.ViewModelFactory
@@ -16,7 +15,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -27,10 +25,11 @@ import androidx.compose.ui.viewinterop.AndroidViewBinding
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 
-class BeforeSearchFragment : Fragment() {
+class OnSearchFragment : Fragment() {
 
-    private var _binding : FragmentBeforeSearchBinding? = null
+    private var _binding : FragmentOnSearchBinding? = null
     private val binding get() = _binding!!
+
 
     //nanti dihapus, ini untuk keperluan data dummy, tapi lagnsung dari API saya tembaknya.. :V
     private lateinit var viewModel: HomeViewModel
@@ -38,93 +37,48 @@ class BeforeSearchFragment : Fragment() {
 
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentBeforeSearchBinding.inflate(inflater, container, false)
+
+        _binding = FragmentOnSearchBinding.inflate(inflater, container, false)
 
         //ini nanti dihpaus, karena supaya agar cuman dapat memiliki data dummy
         viewModel = ViewModelProvider(this, ViewModelFactory.getInstance(requireActivity().application))[HomeViewModel::class.java]
         setupDataDummy()
         //nanti hapus ini yoo
 
+
         return binding.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
+
+
+    override fun onDestroy() {
+        super.onDestroy()
         _binding = null
     }
 
 
     private fun setupDataDummy(){
         viewModel.recommendations.observe(viewLifecycleOwner) {
-            binding.cvLatestSearch.apply {
+            binding.cvSearchResult.apply {
                 setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
                 setContent {
                     MaterialTheme {
 
                         //tinggal ganti disini
-                        SetFoodHistory(it)
+                        SetFoodResult(it)
                     }
                 }
             }
 
-
-            binding.cvFoodRecommended.apply {
-                setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
-                setContent {
-                    MaterialTheme {
-
-                        //tinggal ganti disini
-                        SetFoodRecommendation(it)
-                    }
-                }
-            }
         }
     }
 
 
-
-
     @Composable
-    fun SetFoodHistory(foodList: List<Recipe>){
-        LazyRow(
-            modifier = Modifier
-                .padding(PaddingValues(bottom = 8.dp)),
-            contentPadding = PaddingValues(horizontal = 16.dp),
-            horizontalArrangement = Arrangement.spacedBy(space = 16.dp),
-
-
-        ) {
-
-
-            items(foodList){ item ->
-                AndroidViewBinding(
-                    factory = { layoutInflater, parent, _ ->
-                        ItemFoodCard1Binding.inflate(layoutInflater, parent, false)
-                    },
-
-                    update = {
-                        Glide.with(this.root)
-                            .load(item.image)
-                            .into(this.ivFoodImage)
-                        this.chipFoodCategory.text = item.category.name
-
-                        //nanti set colornya juga
-
-                        this.tvFoodName.text = item.name
-                        this.tvFoodCal.text = getString(R.string.food_cal, item.calories.toInt())
-                    }
-                )
-            }
-        }
-
-    }
-
-    @Composable
-    fun SetFoodRecommendation(foodList: List<Recipe>){
+    fun SetFoodResult(foodList: List<Recipe>){
         LazyColumn(
             modifier = Modifier
                 .padding(PaddingValues(bottom = 8.dp)),
